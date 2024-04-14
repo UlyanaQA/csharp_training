bee00c40d2eae81e33ee454143c7c4923a151591
Кузьмина Ульяна <kuzmina_ua@utisitgroup.ru>
2024-04-14 12:35:48 +0300

    Возврат TearDown, чтоб браузер закрывался

---
+++ addressbook-web-tests/addressbook-web-tests/tests/TestSuiteFixture.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    [SetUpFixture]
+    public class TestSuiteFixture
+    {
+
+        [TearDown]
+        public void StopApplicationManager()
+        {
+            ApplicationManager.GetInstance().Stop();
+        }
+    }
+}
