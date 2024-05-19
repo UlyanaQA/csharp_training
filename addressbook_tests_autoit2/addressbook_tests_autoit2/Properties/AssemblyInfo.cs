51256e591fb90375c5f7b8fbecbaab01286861d7
Кузьмина Ульяна <kuzmina_ua@utisitgroup.ru>
2024-05-19 23:56:05 +0300

    8.1: автотесты десктопов AutoIt

---
+++ addressbook_tests_autoit2/addressbook_tests_autoit2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_autoit2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_autoit2")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b278980-83ba-4587-b4b4-b12a5f134f17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_autoit2/addressbook_tests_autoit2/tests/GroupCreationTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupCreationTests : TestBase
+    {
+
+        [Test]
+        public void TestGroupCreation()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData newGroup = new GroupData()
+            {
+                Name = "test11"
+            };
+            app.Groups.Add(newGroup);
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.Add(newGroup);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups.Count, newGroups.Count);
+        }
+    }
+}
